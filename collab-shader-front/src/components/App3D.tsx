5ac47d897a8568baebc1ffab8153c1a90ff708fd
seb776 <sebastien.maire@epitech.eu>
2025-04-03 19:04:38 +0200

    added a shader

--- collab-shader-front/src/components/App3D.tsx
+++ collab-shader-front/src/components/App3D.tsx
@@ -1,11 +1,50 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { XR, createXRStore } from '@react-three/xr'
+import { useRef } from 'react';
+import * as THREE from 'three'
 
 const store = createXRStore({
     emulate: false,
 })
 
+interface CustomShaderProps {
+    code: string;
+}
+
+const VERTEX_SHADER = /*glsl*/`
+  varying vec2 vUv;
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`;
+
+function CustomShader(props: CustomShaderProps) {
+    const refMat = useRef<any>(undefined);
+    useFrame((state, delta) => {
+        if (refMat && refMat.current) {
+            refMat.current.uniforms.time.value += delta;
+        }
+    });
+    return <>
+        <mesh>
+            <planeGeometry args={[2,2]}/>
+            {/* <meshNormalMaterial/> */}
+            <shaderMaterial ref={refMat} uniforms={{time: {value: 0}}} side={THREE.DoubleSide} vertexShader={VERTEX_SHADER} fragmentShader={props.code}/>
+        </mesh>
+    </>
+}
+
 export function App3D() {
+
+    const fragmentShader = /*glsl*/`
+    uniform float time;
+    uniform vec3 color;
+    varying vec2 vUv;
+    void main() {
+      gl_FragColor.rgba = vec4(0.5 + 0.3 * sin(vUv.yxx + time) + color, 1.0);
+    }
+  `;
     function openSession() {
         store.enterAR()
     }
@@ -14,7 +53,10 @@ export function App3D() {
         <button onClick={openSession}>Enter AR</button>
         <Canvas>
             <XR store={store}>
-                <mesh>
+                <group position={[0,0,-5]}>
+                    <CustomShader code={fragmentShader}/>
+                </group>
+                <mesh position={[5,0,5]}>
                     <boxGeometry args={[2, 2, 2]} />
                     <meshNormalMaterial />
                 </mesh>
